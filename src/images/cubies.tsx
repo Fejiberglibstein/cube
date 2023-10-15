5d6e21aa88e775645214b70a7c199b14c2e30f01
Fejiberglibstein <89747386+Fejiberglibstein@users.noreply.github.com>
2023-10-15 19:57:31 -0400

    Aligned everything to center

--- src/images/cubies.tsx
+++ src/images/cubies.tsx
@@ -1,63 +1,11 @@
-export const src = `
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!-- Created with Inkscape (http://www.inkscape.org/) -->
-
-<svg
-   width="293.6044mm"
-   height="169.44653mm"
-   viewBox="0 0 293.6044 169.44653"
-   version="1.1"
-   id="svg5"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg">
-  <defs
-     id="defs2" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="m 293.50441,84.610407 -48.87893,-28.16987 -48.92256,28.245451 48.87894,28.169752 z"
-     id="22" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="M 244.62585,56.439757 195.74692,28.269874 c -48.92256,28.245453 -48.92291,28.246175 -24.4831,42.330349 24.43911,14.085657 24.43948,14.084874 73.36203,-14.160466 z"
-     id="21" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="M 195.74692,28.269874 171.30745,14.184936 146.868,0.09999845 122.40672,14.222725 97.945425,28.34546 146.82439,56.515234 Z"
-     id="20" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="M 244.58185,112.85575 220.14239,98.770805 c -23.91652,-13.841254 -23.91652,-13.841254 -48.90073,0.03779 -23.93837,14.366375 -23.93837,14.366375 -0.0218,28.207655 l 24.43944,14.08496 24.4613,-14.12274 24.46128,-14.12272"
-     id="12" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="m 171.26346,70.600982 c -24.43947,-14.084956 -48.90075,0.03777 -48.90075,0.03777 -24.461256,14.122731 -0.0218,28.207618 -0.0218,28.207618 24.43947,14.08501 48.90075,-0.03777 48.90075,-0.03777 24.46125,-14.122682 0.0218,-28.207618 0.0218,-28.207618 z"
-     id="11" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="M 97.944799,28.345487 49.022285,56.590914 c 48.878909,28.169765 48.87889,28.169867 73.340175,14.047123 24.46126,-14.122702 24.46126,-14.122702 -24.417661,-42.29255 z"
-     id="10" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="m 195.65927,141.10121 -48.8789,-28.16989 -48.922518,28.24544 48.878928,28.16977 z"
-     id="02" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="M 122.34092,98.846411 C 97.901474,84.761489 97.901474,84.761489 48.978937,113.00692 l 48.878928,28.16975 C 147.45493,112.54179 147.23561,112.66849 122.34092,98.846411 Z"
-     id="01" />
-  <path
-     style="display:inline;opacity:1;mix-blend-mode:normal;fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#b4c1ce;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:fill markers stroke"
-     d="M 97.901438,84.76159 49.022553,56.591705 0.09998999,84.83716 48.978937,113.00692 Z"
-     id="00" />
-</svg>
-`
 export const data = [
-	"m 293.50441,84.610407 -48.87893,-28.16987 -48.92256,28.245451 48.87894,28.169752 z",
-	"M 244.62585,56.439757 195.74692,28.269874 c -48.92256,28.245453 -48.92291,28.246175 -24.4831,42.330349 24.43911,14.085657 24.43948,14.084874 73.36203,-14.160466 z",
-	"M 195.74692,28.269874 171.30745,14.184936 146.868,0.09999845 122.40672,14.222725 97.945425,28.34546 146.82439,56.515234 Z",
-	"M 244.58185,112.85575 220.14239,98.770805 c -23.91652,-13.841254 -23.91652,-13.841254 -48.90073,0.03779 -23.93837,14.366375 -23.93837,14.366375 -0.0218,28.207655 l 24.43944,14.08496 24.4613,-14.12274 24.46128,-14.12272",
-	"m 171.26346,70.600982 c -24.43947,-14.084956 -48.90075,0.03777 -48.90075,0.03777 -24.461256,14.122731 -0.0218,28.207618 -0.0218,28.207618 24.43947,14.08501 48.90075,-0.03777 48.90075,-0.03777 24.46125,-14.122682 0.0218,-28.207618 0.0218,-28.207618 z",
-	"M 97.944799,28.345487 49.022285,56.590914 c 48.878909,28.169765 48.87889,28.169867 73.340175,14.047123 24.46126,-14.122702 24.46126,-14.122702 -24.417661,-42.29255 z",
-	"m 195.65927,141.10121 -48.8789,-28.16989 -48.922518,28.24544 48.878928,28.16977 z",
-	"M 122.34092,98.846411 C 97.901474,84.761489 97.901474,84.761489 48.978937,113.00692 l 48.878928,28.16975 C 147.45493,112.54179 147.23561,112.66849 122.34092,98.846411 Z",
-	"M 97.901438,84.76159 49.022553,56.591705 0.09998999,84.83716 48.978937,113.00692 Z"
+	"M 147.00441,0.11040846 98.12548,-28.059462 49.20293,0.18598846 98.08186,28.355748 Z",
+  "m 98.12585,-28.060242 -48.87892,-28.16989 c -48.92256002,28.24546 -48.92292002,28.24618 -24.4831,42.33035 24.4391,14.08566046 24.43948,14.08488046 73.36202,-14.16046 z",
+	"m 49.24693,-56.230132 -24.43948,-14.08493 -24.43945002,-14.08494 -24.46127998,14.12272 -24.46129,14.12274 48.87895998,28.16977 z",
+	"M 98.08185,28.355748 73.64239,14.270808 C 49.72588,0.42954846 49.72588,0.42954846 24.74166,14.308608 0.80328998,28.674988 0.80328998,28.674988 24.71986,42.516268 l 24.43945,14.08495 24.46129,-14.12273 24.46128,-14.12273",
+	"m 24.76347,-13.899022 c -24.43947002,-14.08495 -48.90075,0.0378 -48.90075,0.0378 -24.46126,14.12273046 -0.0218,28.20762 -0.0218,28.20762 24.43946998,14.08502 48.90075,-0.0378 48.90075,-0.0378 24.46126,-14.12267954 0.0218,-28.20762 0.0218,-28.20762 z",
+	"m -48.5552,-56.154512 -48.92251,28.24542 c 48.87891,28.16977046 48.87889,28.16987046 73.34017,14.04713 24.46126998,-14.12271 24.46126998,-14.12271 -24.41766,-42.29255 z",
+	"M 49.15928,56.601218 0.28037998,28.431328 -48.64214,56.676768 0.23677998,84.846538 Z",
+	"M -24.15908,14.346408 C -48.59852,0.26148846 -48.59852,0.26148846 -97.52106,28.506928 l 48.87893,28.16975 C 0.95492998,28.041798 0.73561998,28.168498 -24.15908,14.346408 Z",
+	"M -48.59856,0.26158846 -97.47744,-27.908292 -146.40001,0.33715846 -97.52106,28.506928 Z"
 ]
