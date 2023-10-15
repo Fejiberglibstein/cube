1bdf5ba6615f158b0c3bc1eb619439347b5b4be0
Fejiberglibstein <89747386+Fejiberglibstein@users.noreply.github.com>
2023-10-15 07:44:14 -0400

    a

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,18 +1,13 @@
-import {Rect, makeScene2D} from '@motion-canvas/2d';
-import {createRefMap, waitFor} from '@motion-canvas/core';
+import {Rect, SVG, makeScene2D} from '@motion-canvas/2d';
+import {createRef, createRefMap, debug, waitFor} from '@motion-canvas/core';
+import src from "../images/cubies.svg"
 
 export default makeScene2D(function* (view) {
-  const cubies = createRefMap<Rect>();
+  const svg = createRef<SVG>();
 
+  debug(src.);
   view.add(
-    <Rect
-      stroke={'white'}
-      width={100}
-      height={100}
-      lineWidth={3}
-      rotation={-45}
-      // scaleX={0.5}
-    ></Rect>,
+    <SVG svg={src}></SVG>
   );
 
   yield* waitFor(5);
